cf125d07c91745466e26bd71066442252fec7ceb
jacobfain <jfain@chicagobooth.edu>
2021-05-25 22:57:08 +0000

    make user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,11 @@ class UsersController < ApplicationController
     @users = User.all.order({:username => :asc})
     render({:template => "users/index.html.erb"})
   end
+
+  def show
+    the_username = params.fetch("path_username")
+    matching_user = User.where({:username => the_username})
+    @user = matching_user.at(0)
+    render({:template => "users/show.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -107,6 +107,7 @@ Rails.application.routes.draw do
   #INDEX
 
   get("/users", {:controller => "users", :action => "index"})
+  get("/users/:path_username", {:controller => "users", :action => "show"})
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
